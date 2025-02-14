d5f614f423f8f6186881035f53581550067aca02
skapersk <simon.kaperski@gmail.com>
2025-02-14 15:49:49 +0100

    boilerplat for hackathon done

--- srcs/backend/app/main.py
+++ srcs/backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import engine
 from app import models
 from app.routers import users, items
@@ -8,10 +9,18 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+# Ajout du middleware CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Inclusion des routes
 app.include_router(users.router)
 app.include_router(items.router)
 
-@app.get("/")
+@app.get("/api/")
 def root():
-    return {"message": "Bienvenue sur FastAPI avec PostgreSQL !"}
+	return {"message": "Bienvenue sur FastAPI avec PostgreSQL !"}
---
+++ srcs/frontend/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react'
+import axios from "axios";
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:8000/api/") // Appel à l'API backend FastAPI
+      .then((response) => {
+        setMessage(response.data.message);
+      })
+      .catch((error) => {
+        console.error("Erreur lors de la récupération des données", error);
+      });
+  }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+		<div className="p-4">
+      		<h1 className="text-2xl font-bold">Bienvenue</h1>
+      		<p className="mt-4 text-lg">{message}</p>
+    	</div>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
